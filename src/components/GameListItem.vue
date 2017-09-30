f38012824dfb8a134589cc3d37276c1574602a98
Matthew Hummer <biggreyson@gmail.com>
2017-09-30 15:20:12 -0600

    fetch and render all game titles

---
+++ src/components/GameListItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{game.title}}</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: ['game', 'index'],
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+
+<style>
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,20 +3,36 @@
     <Navbar></Navbar>
     <div class = "content">
       <h1>ALL GAMES</h1>
+      <GameListItem v-for="(game, index) in games" :key="index" :game="game" :index="index"></GameListitem>
     </div>
   </div>
 </template>
+
+
 <script>
 import Navbar from '@/components/Navbar.vue'
+import GameListItem from '@/components/GameListItem.vue'
+import axios from 'axios'
+
 export default {
   name: 'home',
-  components: {Navbar},
+  components: {Navbar, GameListItem},
   created () {
-    console.log('meow!')
+    axios({
+      url: 'http://localhost:8000/games',
+      method: 'GET',
+      headers: {
+        Authorization: 'Basic bWF0dGFsdWk6cGFzc3dvcmQ='
+      }
+    })
+      .then((response) => {
+        this.games = response.data
+      })
   },
   data () {
     return {
-      msg: 'You have done it!'
+      msg: 'You have done it!',
+      games: []
     }
   }
 }
@@ -26,7 +42,7 @@ export default {
 <style scoped>
 .content{
   width: 80%;
-  height: 200vh;
+  min-height: 100vh;
   /*background-color: rgb(199,204,215);*/
   background-color: rgb(239,244,255);
   margin-top: 100px;
